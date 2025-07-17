f78a7cfbdd30c7fd6fafd3a69e69284e64b5eb07
Suryaprakash20038 <ssathiskumar641@gmail.com>
2025-07-17 14:03:24 +0530

    Add flightData source

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -6,7 +6,6 @@ import SearchSection from './Searchsection';
 import FilterPanel from './FilterPanel';
 import FlightList from './Flightlist';
 import './HomePage.css';
-import data from "./data";
 
 
 function HomePage() {
